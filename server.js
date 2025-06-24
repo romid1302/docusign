3fa20eec003ca761eb0fdbee8542cb9b350088b8
Deepanshu Kumar <136815366+romid1302@users.noreply.github.com>
2025-06-24 10:56:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 
 
-app.use(express.json());
+
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
